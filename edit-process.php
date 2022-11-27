55a5c471fb38d194405c743d35ffe4bc69258e3a
Emmanuel Ugaban <ugabane0516@gmail.com>
2022-11-27 13:50:55 +0800

    private data

--- edit-process.php
+++ edit-process.php
@@ -1,23 +1,31 @@
 <?php
-    include_once 'database.php';
-    if(isset($_POST['save']))
-        {
-            $fname = $_POST['f_name'];
-            $lname = $_POST['l_name'];
-            $email = $_POST['email'];
-            $contact = $_POST['contact'];
-            $address = $_POST['address'];   
-            $app_date = $_POST['date'];
-            $app_time = $_POST['time'];
-            $id = $_POST['id'];
+session_start();
 
-            //query
-            $sql = "UPDATE `reservation` SET `f_name`='$fname',`l_name`='$lname',`email`='$email',
-            `contact_no`='$contact',`address`='$address', `app_date`='$app_date',`app_time`='$app_time' 
-            WHERE id = $id";
+if (isset($_SESSION["user_id"]))
+    {
+        include_once 'database.php';
+        if(isset($_POST['save']))
+            {
+                $fname = $_POST['f_name'];
+                $lname = $_POST['l_name'];
+                $email = $_POST['email'];
+                $contact = $_POST['contact'];
+                $address = $_POST['address'];   
+                $app_date = $_POST['date'];
+                $app_time = $_POST['time'];
+                $id = $_POST['id'];
 
-            //updating
-            $updating = mysqli_query($mysqli, $sql);
+                //query
+                $sql = "UPDATE `reservation` SET `f_name`='$fname',`l_name`='$lname',`email`='$email',
+                `contact_no`='$contact',`address`='$address', `app_date`='$app_date',`app_time`='$app_time' 
+                WHERE id = $id";
 
-            header("location: view-row.php?id=$id");
-        }
+                //updating
+                $updating = mysqli_query($mysqli, $sql);
+
+                header("location: view-row.php?id=$id");
+            }
+    }
+else {
+    echo "<h1>Error</h1>";
+}
